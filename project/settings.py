7ec72c3063a684ca0b7d3b550d2141d77d4fe226
Hadi Asghari <hadiasghari@users.noreply.github.com>
2021-05-14 23:41:58 +0200

    added memcache support for heroku

--- project/settings.py
+++ project/settings.py
@@ -154,3 +154,53 @@ if 'django_heroku' in sys.modules:
 	DEBUG_PROPAGATE_EXCEPTIONS = True
 	django_heroku.settings(locals())
 	 # ignore locally if module is not installed (hence not imported)
+
+
+# HA Setup MemCache for Heroku
+# ref: https://devcenter.heroku.com/articles/django-memcache#create-a-django-application-for-heroku
+def get_cache():
+  import os
+  try:
+    servers = os.environ['MEMCACHIER_SERVERS']
+    username = os.environ['MEMCACHIER_USERNAME']
+    password = os.environ['MEMCACHIER_PASSWORD']
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None` disables expiration.
+        'TIMEOUT': None,
+        'LOCATION': servers,
+        'OPTIONS': {
+          'binary': True,
+          'username': username,
+          'password': password,
+          'behaviors': {
+            # Enable faster IO
+            'no_block': True,
+            'tcp_nodelay': True,
+            # Keep connection alive
+            'tcp_keepalive': True,
+            # Timeout settings
+            'connect_timeout': 2000, # ms
+            'send_timeout': 750 * 1000, # us
+            'receive_timeout': 750 * 1000, # us
+            '_poll_timeout': 2000, # ms
+            # Better failover
+            'ketama': True,
+            'remove_failed': 1,
+            'retry_timeout': 2,
+            'dead_timeout': 30,
+          }
+        }
+      }
+    }
+  except:
+    return {
+	  # for development mode where `memcachier` doesn't exist
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
+
+CACHES = get_cache()
