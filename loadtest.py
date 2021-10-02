efe97ce4d4e41e87e6e6c37d5e32ebce76b13ed1
Hadi <hadiasghari@users.noreply.github.com>
2021-10-02 15:54:33 +0200

    loadtest loops

--- loadtest.py
+++ loadtest.py
@@ -13,42 +13,67 @@ from selenium import webdriver
 from random import randint
 from time import sleep
 
-browser = webdriver.Chrome()
 
-# lets start
-browser.get("http://calledearth.herokuapp.com")
-
-# pick emoji
-ptxt = browser.find_element_by_tag_name("body").text
-assert "Pick your emoji" in ptxt
-elems = browser.find_elements_by_tag_name("span")
-emoji = elems[randint(0, len(elems)-1)]
-emoji.click()
-
-# browser tag
-tag = randint(100, 999)
-n = 0
-
-# maybe only continue for X seconds, then exit...
 while True:
-    ptxt = browser.find_element_by_tag_name("body").text
-    if "feeling" in ptxt or 'DANCE!' in ptxt or 'more' in ptxt:
-        # cheer prompt, send random energy
-        elems = browser.find_elements_by_tag_name("img")
-        powerup = elems[randint(0, len(elems)-1)]
-        powerup.click()
-    else:
-        # writing prompt. send a random hello
-        try:
-            txtarea = browser.find_element_by_tag_name("textarea")
-        except ex:
-            print(ex)  # ended or unexpected, quit
-            break
-        n += 1
-        txtarea.send_keys("hello friends #" + str(tag) + "." + str(n) )
-        f = browser.find_element_by_tag_name("form")
-        f.submit()
-    # sleep depending on intensity!
-    sleep(2)
+    browser = webdriver.Chrome()
 
-browser.close()
+    # lets start
+    print("Connecting!")
+    browser.get("http://calledearth.herokuapp.com")
+    #browser.get("http://127.0.0.1:8000")
+
+    # pick emoji
+    ptxt = browser.find_element_by_tag_name("body").text
+    assert "Pick your emoji" in ptxt
+    elems = browser.find_elements_by_tag_name("span")
+    emoji = elems[randint(0, len(elems)-1)]
+    print("Our emoji is ", str(emoji.text))
+    emoji.click()
+
+    # browser tag
+    tag = randint(100, 999)
+    n = 0
+    bye = False
+
+    # maybe only continue for X seconds, then exit...
+    while True:
+        try:
+            ptxt = browser.find_element_by_tag_name("body").text
+        except Exception as ex:
+            print(ex)
+            break
+        if "feeling" in ptxt or 'DANCE!' in ptxt or 'more' in ptxt:
+            # cheer prompt, send random energy
+            elems = browser.find_elements_by_tag_name("img")
+            if not elems:
+                bye = True
+                print("THE END!")
+                break  # we must have reached the end!
+            powerup = elems[randint(0, len(elems)-1)]
+            try:
+                print("Power Up!")
+                powerup.click()
+            except Exception as ex:
+                print(ex)
+                break
+        else:
+            # writing prompt. send a random hello
+            try:
+                txtarea = browser.find_element_by_tag_name("textarea")
+            except Exception as ex:
+                print(ex)  # ended or unexpected, quit
+                break
+            n += 1
+            print("Texting!")
+            txtarea.send_keys("hello bot friends #" + str(tag) + "." + str(n) )
+            f = browser.find_element_by_tag_name("form")
+            f.submit()
+        # sleep depending on intensity!
+        sleep(2)
+
+    browser.close()
+
+    if bye:
+        break
+
+    sleep(10)
